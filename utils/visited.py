d94d6b91543e676eb21b3b6fd7b9e3cbdb923d77
xolarvill <since1963@qq.com>
2025-03-30 17:27:38 +0800

    删除未使用的Hessian计算和参数结果类，添加访问地点序列获取功能

--- utils/visited.py
+++ utils/visited.py
@@ -1,2 +1,6 @@
-def visited_sequence():
-    return
+import pandas as pd
+
+def visited_sequence(pid: int, data: pd.DataFrame):
+    # 获取pid的访问地点序列，并按year升序排列
+    result = data[data['pid'] == pid][['provcd', 'year']].sort_values(by='year')
+    return result
